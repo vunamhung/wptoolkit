592eef54756e10714f19a1b6d1d5ac4a88e6e6a5
Hung Vu <vunamhung@gmail.com>
2020-02-21 15:29:42 +0700

    add kses, svg

--- helpers/general.php
+++ helpers/general.php
@@ -17,3 +17,28 @@ function flatten_version($version) {
 
 	return implode('', $parts);
 }
+
+function get_svg_icon($args) {
+	if (!is_array($args)) {
+		$args = ['icon' => $args];
+	}
+
+	$args = wp_parse_args($args, [
+		'icon' => 'email',
+		'title' => '',
+		'desc' => '',
+		'aria_hidden' => true, // Hide from screen readers.
+	]);
+
+	$aria_hidden = $args['aria_hidden'] === true ? 'aria-hidden="true"' : '';
+	$aria_labelledby = $args['title'] && $args['desc'] ? 'aria-labelledby="title desc"' : '';
+
+	// Begin SVG markup.
+	$svg = sprintf('<svg class="icon %1$s" %2$s %3$s role="img">', $args['icon'], $aria_hidden, $aria_labelledby);
+	$svg .= $args['title'] ? sprintf('<title>%s</title>', $args['title']) : '';
+	$svg .= $args['desc'] ? sprintf('<desc>%s</desc>', $args['desc']) : '';
+	$svg .= sprintf('<use xlink:href="#%s"></use>', $args['icon']);
+	$svg .= '</svg>';
+
+	return wp_kses($svg, 'svg');
+}
